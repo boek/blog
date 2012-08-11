8ae5868a58949157a3b84507b7ff3cde123d2512
Jeff Boek <j@jboek.com>
2012-08-11 02:25:18 -0700

    added test

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,15 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "blog"
+set :repository,  "git@github.com:jeffboek/blog.git"
 
 set :scm, :git
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "jeffboek.com"                          # Your HTTP server, Apache/etc
+role :app, "jeffboek.com"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+set :deploy_to, "/home/jeffboek/#{application}"
+set :use_sudo, false
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
